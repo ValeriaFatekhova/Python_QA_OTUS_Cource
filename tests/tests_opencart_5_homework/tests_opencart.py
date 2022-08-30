fdbd8fbeb1ba2a4bd953b1f53ef3e56f9e09a101
ruvkuminov <Valeria.Fatekhova@harman.com>
2022-08-30 23:18:32 +0300

    add user login page tests

--- tests/tests_opencart_5_homework/tests_opencart.py
+++ tests/tests_opencart_5_homework/tests_opencart.py
@@ -47,238 +47,238 @@ def test_home_page_content(driver, url):
     home_page.is_content()
 
 
-# """Catalog page tests"""
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "component/scanner",
-#         "component/monitor",
-#     ]
-# )
-# def test_catalog_left_menu(driver, url, add_to_url):
-#     url = url + add_to_url
-#     catalog_page = CatalogPage(driver)
-#     catalog_page.open_catalog_page(url)
-#     catalog_page.is_left_menu()
-#     active_items = catalog_page.get_active_menu_items()
-#     expected_items = add_to_url.split("/")
-#     for i in range(len(expected_items)):
-#         if expected_items[i].lower() in active_items[i].lower():
-#             continue
-#         else:
-#             raise AssertionError(f"Incorrect active items in left menu")
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "component/scanner",
-#         "component/monitor",
-#     ]
-# )
-# def test_catalog_content(driver, url, add_to_url):
-#     url = url + add_to_url
-#     catalog_page = CatalogPage(driver)
-#     catalog_page.open_catalog_page(url)
-#     catalog_page.is_catalog_content()
-#     if catalog_page.get_active_menu_items()[-1].find("0") != -1:
-#         catalog_page.is_catalog_empty()
-#     else:
-#         assert str(len(catalog_page.get_products())) in catalog_page.get_active_menu_items()[-1]
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "component/scanner",
-#         "component/monitor",
-#     ]
-# )
-# def test_catalog_breadcrumbs(driver, url, add_to_url):
-#     url = url + add_to_url
-#     catalog_page = CatalogPage(driver)
-#     catalog_page.open_catalog_page(url)
-#     catalog_page.is_breadcrumbs()
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "component/scanner",
-#         "component/monitor",
-#     ]
-# )
-# def test_catalog_continue_button(driver, url, add_to_url):
-#     url = url + add_to_url
-#     catalog_page = CatalogPage(driver)
-#     catalog_page.open_catalog_page(url)
-#     if catalog_page.get_active_menu_items()[-1].find("0") != -1:
-#         catalog_page.is_continue_button()
-#     else:
-#         try:
-#             catalog_page.is_continue_button()
-#         except AssertionError:
-#             pass
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "component/scanner",
-#         "component/monitor",
-#     ]
-# )
-# def test_list_view_button(driver, url, add_to_url):
-#     url = url + add_to_url
-#     catalog_page = CatalogPage(driver)
-#     catalog_page.open_catalog_page(url)
-#     if catalog_page.get_active_menu_items()[-1].find("0") != -1:
-#         try:
-#             catalog_page.is_list_view_button()
-#         except AssertionError:
-#             pass
-#     else:
-#         catalog_page.is_list_view_button()
-#
-#
-# """Product page tests"""
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "monitor/test",
-#         "laptop-notebook/hp-lp3065",
-#     ]
-# )
-# def test_product_thumbnails(driver, url, add_to_url):
-#     url = url + add_to_url
-#     product_page = ProductPage(driver)
-#     product_page.open_product_page(url)
-#     product_page.is_thumbnails()
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "monitor/test",
-#         "laptop-notebook/hp-lp3065",
-#     ]
-# )
-# def test_product_description(driver, url, add_to_url):
-#     url = url + add_to_url
-#     product_page = ProductPage(driver)
-#     product_page.open_product_page(url)
-#     product_page.is_product_description()
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "monitor/test",
-#         "laptop-notebook/hp-lp3065",
-#     ]
-# )
-# def test_product_review(driver, url, add_to_url):
-#     url = url + add_to_url
-#     product_page = ProductPage(driver)
-#     product_page.open_product_page(url)
-#     product_page.is_product_review()
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "monitor/test",
-#         "laptop-notebook/hp-lp3065",
-#     ]
-# )
-# def test_add_to_cart_button(driver, url, add_to_url):
-#     url = url + add_to_url
-#     product_page = ProductPage(driver)
-#     product_page.open_product_page(url)
-#     product_page.is_add_to_cart_button()
-#
-#
-# @pytest.mark.parametrize(
-#     "add_to_url",
-#     [
-#         "monitor/test",
-#         "laptop-notebook/hp-lp3065",
-#     ]
-# )
-# def test_product_rating(driver, url, add_to_url):
-#     url = url + add_to_url
-#     product_page = ProductPage(driver)
-#     product_page.open_product_page(url)
-#     product_page.is_rating()
-#
-#
-# """Admin login page tests"""
-#
-#
-# def test_admin_login_page_logo(driver, url):
-#     admin_page = AdminLoginPage(driver)
-#     admin_page.open_admin_login_page(url)
-#     admin_page.is_admin_logo()
-#     admin_page.check_logo_link()
-#
-#
-# def test_al_username_field(driver, url):
-#     admin_page = AdminLoginPage(driver)
-#     admin_page.open_admin_login_page(url)
-#     admin_page.is_user_name_field()
-#
-#
-# def test_al_password_field(driver, url):
-#     admin_page = AdminLoginPage(driver)
-#     admin_page.open_admin_login_page(url)
-#     admin_page.is_password_field()
-#
-#
-# def test_al_login_button(driver, url):
-#     admin_page = AdminLoginPage(driver)
-#     admin_page.open_admin_login_page(url)
-#     admin_page.is_login_button()
-#
-#
-# def test_al_forgotten_password_link(driver, url):
-#     admin_page = AdminLoginPage(driver)
-#     admin_page.open_admin_login_page(url)
-#     admin_page.is_forgotten_password_link()
+"""Catalog page tests"""
 
 
-# """Admin login page tests"""
-#
-#
-# def test_ul_firstname_field(driver, url):
-#     user_page = UserLoginPage(driver)
-#     user_page.open_user_login_page(url)
-#     user_page.is_first_name_field()
-#
-#
-# def test_ul_lastname_field(driver, url):
-#     user_page = UserLoginPage(driver)
-#     user_page.open_user_login_page(url)
-#     user_page.is_first_name_field()
-#
-#
-# def test_ul_email_field(driver, url):
-#     user_page = UserLoginPage(driver)
-#     user_page.open_user_login_page(url)
-#     user_page.is_email_field()
-#
-#
-# def test_ul_right_menu(driver, url):
-#     user_page = UserLoginPage(driver)
-#     user_page.open_user_login_page(url)
-#     user_page.is_right_menu()
-#     user_page.check_menu_items(user_page.get_menu_items())
-#
-#
-# def test_ul_continue_button(driver, url):
-#     user_page = UserLoginPage(driver)
-#     user_page.open_user_login_page(url)
-#     user_page.is_continue_button()
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "component/scanner",
+        "component/monitor",
+    ]
+)
+def test_catalog_left_menu(driver, url, add_to_url):
+    url = url + add_to_url
+    catalog_page = CatalogPage(driver)
+    catalog_page.open_catalog_page(url)
+    catalog_page.is_left_menu()
+    active_items = catalog_page.get_active_menu_items()
+    expected_items = add_to_url.split("/")
+    for i in range(len(expected_items)):
+        if expected_items[i].lower() in active_items[i].lower():
+            continue
+        else:
+            raise AssertionError(f"Incorrect active items in left menu")
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "component/scanner",
+        "component/monitor",
+    ]
+)
+def test_catalog_content(driver, url, add_to_url):
+    url = url + add_to_url
+    catalog_page = CatalogPage(driver)
+    catalog_page.open_catalog_page(url)
+    catalog_page.is_catalog_content()
+    if catalog_page.get_active_menu_items()[-1].find("0") != -1:
+        catalog_page.is_catalog_empty()
+    else:
+        assert str(len(catalog_page.get_products())) in catalog_page.get_active_menu_items()[-1]
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "component/scanner",
+        "component/monitor",
+    ]
+)
+def test_catalog_breadcrumbs(driver, url, add_to_url):
+    url = url + add_to_url
+    catalog_page = CatalogPage(driver)
+    catalog_page.open_catalog_page(url)
+    catalog_page.is_breadcrumbs()
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "component/scanner",
+        "component/monitor",
+    ]
+)
+def test_catalog_continue_button(driver, url, add_to_url):
+    url = url + add_to_url
+    catalog_page = CatalogPage(driver)
+    catalog_page.open_catalog_page(url)
+    if catalog_page.get_active_menu_items()[-1].find("0") != -1:
+        catalog_page.is_continue_button()
+    else:
+        try:
+            catalog_page.is_continue_button()
+        except AssertionError:
+            pass
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "component/scanner",
+        "component/monitor",
+    ]
+)
+def test_list_view_button(driver, url, add_to_url):
+    url = url + add_to_url
+    catalog_page = CatalogPage(driver)
+    catalog_page.open_catalog_page(url)
+    if catalog_page.get_active_menu_items()[-1].find("0") != -1:
+        try:
+            catalog_page.is_list_view_button()
+        except AssertionError:
+            pass
+    else:
+        catalog_page.is_list_view_button()
+
+
+"""Product page tests"""
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "monitor/test",
+        "laptop-notebook/hp-lp3065",
+    ]
+)
+def test_product_thumbnails(driver, url, add_to_url):
+    url = url + add_to_url
+    product_page = ProductPage(driver)
+    product_page.open_product_page(url)
+    product_page.is_thumbnails()
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "monitor/test",
+        "laptop-notebook/hp-lp3065",
+    ]
+)
+def test_product_description(driver, url, add_to_url):
+    url = url + add_to_url
+    product_page = ProductPage(driver)
+    product_page.open_product_page(url)
+    product_page.is_product_description()
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "monitor/test",
+        "laptop-notebook/hp-lp3065",
+    ]
+)
+def test_product_review(driver, url, add_to_url):
+    url = url + add_to_url
+    product_page = ProductPage(driver)
+    product_page.open_product_page(url)
+    product_page.is_product_review()
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "monitor/test",
+        "laptop-notebook/hp-lp3065",
+    ]
+)
+def test_add_to_cart_button(driver, url, add_to_url):
+    url = url + add_to_url
+    product_page = ProductPage(driver)
+    product_page.open_product_page(url)
+    product_page.is_add_to_cart_button()
+
+
+@pytest.mark.parametrize(
+    "add_to_url",
+    [
+        "monitor/test",
+        "laptop-notebook/hp-lp3065",
+    ]
+)
+def test_product_rating(driver, url, add_to_url):
+    url = url + add_to_url
+    product_page = ProductPage(driver)
+    product_page.open_product_page(url)
+    product_page.is_rating()
+
+
+"""Admin login page tests"""
+
+
+def test_admin_login_page_logo(driver, url):
+    admin_page = AdminLoginPage(driver)
+    admin_page.open_admin_login_page(url)
+    admin_page.is_admin_logo()
+    admin_page.check_logo_link()
+
+
+def test_al_username_field(driver, url):
+    admin_page = AdminLoginPage(driver)
+    admin_page.open_admin_login_page(url)
+    admin_page.is_user_name_field()
+
+
+def test_al_password_field(driver, url):
+    admin_page = AdminLoginPage(driver)
+    admin_page.open_admin_login_page(url)
+    admin_page.is_password_field()
+
+
+def test_al_login_button(driver, url):
+    admin_page = AdminLoginPage(driver)
+    admin_page.open_admin_login_page(url)
+    admin_page.is_login_button()
+
+
+def test_al_forgotten_password_link(driver, url):
+    admin_page = AdminLoginPage(driver)
+    admin_page.open_admin_login_page(url)
+    admin_page.is_forgotten_password_link()
+
+
+"""Admin login page tests"""
+
+
+def test_ul_firstname_field(driver, url):
+    user_page = UserLoginPage(driver)
+    user_page.open_user_login_page(url)
+    user_page.is_first_name_field()
+
+
+def test_ul_lastname_field(driver, url):
+    user_page = UserLoginPage(driver)
+    user_page.open_user_login_page(url)
+    user_page.is_first_name_field()
+
+
+def test_ul_email_field(driver, url):
+    user_page = UserLoginPage(driver)
+    user_page.open_user_login_page(url)
+    user_page.is_email_field()
+
+
+def test_ul_right_menu(driver, url):
+    user_page = UserLoginPage(driver)
+    user_page.open_user_login_page(url)
+    user_page.is_right_menu()
+    user_page.check_menu_items(user_page.get_menu_items())
+
+
+def test_ul_continue_button(driver, url):
+    user_page = UserLoginPage(driver)
+    user_page.open_user_login_page(url)
+    user_page.is_continue_button()
